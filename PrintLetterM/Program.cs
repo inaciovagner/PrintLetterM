72b8f3541e429e90fb1806237e22e2451e78faac
Vagner de Souza <inaciovagner1@gmail.com>
2021-03-29 16:45:29 +0100

    Add project files.

---
+++ PrintLetterM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintLetterM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            int x;
+            for (x = 0; x < 10; x++)
+            {
+                for (y = 5; y > 0; y--)
+                {
+                    Console.WriteLine("M");
+                }
+               
+            }
+        }
+    }
+}
